d7f31e405b08e20dbd4b349ad47f54387fcd1a22
iwtw <wwttww@qq.com>
2017-05-10 16:39:35 +0800

    modified:   vgg.sh 	modified:   vgg19.py

---
+++ printvar.py
@@ -0,0 +1,8 @@
+from tensorflow.python import pywrap_tensorflow  
+import os
+model_dir = "checkpoint/sr_res_vgg_p=0.5_5_10_16/"
+checkpoint_path = os.path.join(model_dir, "srwgan")  
+reader = pywrap_tensorflow.NewCheckpointReader(checkpoint_path)  
+var_to_shape_map = reader.get_variable_to_shape_map()  
+for key in var_to_shape_map:  
+    print("tensor_name: ", key)  
--- vgg.sh
+++ vgg.sh
@@ -1,4 +1,4 @@
 #!bin/bash
-p=0.9
+p=0.5
 name=sr_res_vgg_"p=$p"_5_10_16
 nohup python -u sr_res_vgg22.py $name $p >> info.$name &
